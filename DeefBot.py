7d774bd4b805b4460fc8cd43bd050e9ea66122f2
Dave Roverts <me@daveroverts.nl>
2018-05-20 14:48:08 +0200

    Adds more tableflips in a array, randomly chooses one on each call.

--- DeefBot.py
+++ DeefBot.py
@@ -3,10 +3,12 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
+import random
 
 Client = discord.Client()
 client = commands.Bot(command_prefix='!')
 
+tables = ["(╯°□°）╯︵ ┻━┻", "┬──┬﻿ ¯\_(ツ)", "┻━┻ ︵ヽ(`Д´)ﾉ︵﻿ ┻━┻", "┻━┻ ︵﻿ ¯\(ツ)/¯ ︵ ┻━┻", "┬─┬ノ( º _ ºノ)", "(ノಠ益ಠ)ノ彡┻━┻"]
 @client.event
 async def on_ready():
     print("Bot is ready")
@@ -17,6 +19,6 @@ async def on_message(message):
     if message.content == "mand":
         await client.send_message(message.channel, "MAND!")
     elif message.content == "flip":
-        await client.send_message(message.channel, "(╯°□°）╯︵ ┻━┻")
+        await client.send_message(message.channel, random.choice(tables))
 
 client.run('erm..... get your own token!')
